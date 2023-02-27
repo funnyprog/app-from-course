a698fd40bfa53449457511ed5d62b5b00d418ad9
funnyprog <zabotin7513@mail.ru>
2023-02-27 20:56:29 +0300

    fix tests error with router

--- src/shared/lib/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/renderWithTranslation/renderWithTranslation.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import { i18nForTests } from 'shared/config/i18nForTests';
 
 export const renderWithTranslation = (component: ReactNode) => render(
     <I18nextProvider i18n={i18nForTests}>
-        {component}
+        <MemoryRouter>
+            {component}
+        </MemoryRouter>
     </I18nextProvider>,
 );
